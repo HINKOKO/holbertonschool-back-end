adb9d1694504180a0bc323a9c14a531e5a7d3532
root <piotr62@live.fr>
2022-11-29 04:12:36 -0800

    Task 3 Let's see what's happen

---
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+Script to gather data from an API
+Export in JSON format, all tasks from all employees!
+Party time !!
+==> "requests" module allows us to send HTTP Requests
+The request returns a Response Object with all data
+(content, encoding, status, etc...)
+"""
+import json
+import requests
+from sys import argv
+
+response_API = 'https://jsonplaceholder.typicode.com/'
+if __name__ == "__main__":
+    users = requests.get("{}users".format(response_API)).json()
+    # todos = requests.get("{}todos".format(
+    #     response_API)).json()
+
+    dict_json = {}
+    list_id = [u.get('id') for u in users]
+    for ids in list_id:
+        users_id = requests.get(
+            "https://jsonplaceholder.typicode.com/users?id:ids").json()
+        # print(users_id)
+        user = users_id[0].get("username")
+        users_task = requests.get(
+            "https://jsonplaceholder.typicode.com/todos?userId:ids").json()
+        # print(user)
+    t_list = []
+    t_dict = {}
+    for t in users_task:
+        t_dict = {"username": user,
+                  "task": t.get('title'),
+                  "completed": t.get('completed')
+                  }
+        t_list.append(t_dict)
+        dict_json = {}
+        dict_json.update({ids: t_list})
+        with open("todo_all_employees.json", 'w') as jsonfile:
+            json.dump(dict_json, jsonfile)
+
+    # print(users)
+    # print(todos)
+
+    # with open("todo_all_employees.json", 'w') as jsonfile:
+    #     json.dump(dict_json, jsonfile)
