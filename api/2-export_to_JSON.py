6e621d8c1e8c00f0144cfdb2910d8af0e0a10b9b
root <piotr62@live.fr>
2022-11-29 02:21:44 -0800

    Hunting the error !

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -22,10 +22,10 @@ if __name__ == "__main__":
     for t in todos:
         t_dict = {"task": t.get("title"),
                   "completed": t.get("completed"),
-                  "username": users
+                  "username": users.get("username")
                   }
         t_list.append(t_dict)
         dict_json = {}
-        dict_json[argv[1]] = t_list
+        dict_json[(argv[1])] = t_list
         with open("{}.json".format(argv[1]), 'w') as jsonfile:
             json.dump(dict_json, jsonfile)
